5ee923c457520758e8613615c05ecf3ced510099
xiaowu <94637401+mwx280@users.noreply.github.com>
2026-07-29 23:44:58 +0800

    Initial Commit

---
+++ Aria2Desk/Aria2DeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aria2DeskApp.swift
+//  Aria2Desk
+//
+//  Created by xiaowu on 2026/7/29.
+//
+
+import SwiftUI
+
+@main
+struct Aria2DeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
